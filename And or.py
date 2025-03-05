b49d523fcf49eb2aa3fb31fcffcc33a5fbb8f10c
AliZain <alizain.vibes@gmail.com>
2025-03-05 17:27:52 +1100

    And Or operatort

---
+++ And or.py
@@ -0,0 +1,11 @@
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("either of the number is greater then 0")
+else:
+    print("no number is grater then 0")
+if a>0 and b>0:
+    print("either of the number is greater then 0")
+else:
+    print("no number is grater then 0")
